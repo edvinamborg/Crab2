44bca94f413b0a1ee8efb84107e2868f598b251a
PostisPer <edvin.amborg@gmail.com>
2023-04-28 10:56:28 +0200

    GameOver/vinst-skärm

--- Assets/Scripts/Crab.cs
+++ Assets/Scripts/Crab.cs
@@ -4,20 +4,25 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(CapsuleCollider2D))]
 
 public class Crab : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private float speed = 4;
+    private float speed = 6;
     public TMP_Text scoreDisplay;
     private TMP_Text scoreDisplaytext;
     private int score = 0;
     public TMP_Text livDisplay;
-    public TMP_Text GameOver;
     private TMP_Text livDisplaytext;
     private int Liv = 3;
+    public TMP_Text gameover;
+    public TMP_Text restart;
+    public TMP_Text vinst;
+  
+   
     
     
     // Start is called before the first frame update
@@ -30,10 +35,21 @@ public class Crab : MonoBehaviour
         rb.gravityScale = 0;
         scoreDisplaytext = scoreDisplay.GetComponent<TMP_Text>();
         livDisplaytext = livDisplay.GetComponent<TMP_Text>();
+        gameover.enabled = false;
+        restart.enabled = false;
+        vinst.enabled = false;
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene("SampleScene");
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.S))
         {
@@ -46,12 +62,12 @@ public class Crab : MonoBehaviour
             if (Input.GetKey(KeyCode.A))
             {
                 // and turn left
-                rb.rotation += 1;
+                rb.rotation += 4;
             }
             else if (Input.GetKey(KeyCode.D))
             {
                 // and turn right
-                rb.rotation -= 1;
+                rb.rotation -= 4;
             }
         }
         else
@@ -59,6 +75,8 @@ public class Crab : MonoBehaviour
             // do not move
             rb.velocity = Vector2.zero;
         }
+
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -70,6 +88,13 @@ public class Crab : MonoBehaviour
             Destroy(collision.gameObject);
             score = score + 1;
             scoreDisplaytext.text = "Score: " + score;
+
+            if (score == 6)
+            {
+                Time.timeScale = 0;
+                vinst.enabled = true;
+                restart.enabled = true;
+            }
         }
 
         else if (collision.gameObject.name.StartsWith("lobster"))
@@ -78,6 +103,15 @@ public class Crab : MonoBehaviour
             Liv = Liv - 1;
             Destroy(collision.gameObject);
             livDisplay.text = "Liv: " + Liv;
+
+            if (Liv == 0)
+            {
+                Time.timeScale = 0;
+                gameover.enabled = true;
+                restart.enabled = true;
+            }
+
+
 
         }
     }
